686322767aee9714a855a2392c14e21004363017
丶左手 <87903717+iZuoShou@users.noreply.github.com>
2025-04-30 01:18:06 +0800

    Delete Rewrite/github-429.js

--- Rewrite/github-429.js
+++
@@ -1,9 +0,0 @@
-# 解决github更新资源429问题
-# 2025-04-30
-  
-[rewrite]
-^https:\/\/(raw|gist)\.githubusercontent\.com\/ header-replace Accept-Language en-us
-^https:\/\/github\.com\/ header-replace Accept-Language en-us
-
-[Mitm]
-hostname = raw.githubusercontegithubnt.com,gist.githubusercontent.com,github.com
