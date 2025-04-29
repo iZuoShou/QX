0f272a46cf43db0dd28bed16ba05b38f11f31d6f
丶左手 <87903717+iZuoShou@users.noreply.github.com>
2025-04-30 01:11:35 +0800

    Create github-429.js

---
+++ Rewrite/github-429.js
@@ -0,0 +1,9 @@
+# 解决github更新资源429问题
+# 2025-04-30
+  
+[rewrite_local]
+^https:\/\/(raw|gist)\.githubusercontent\.com\/ header-replace Accept-Language en-us
+^https:\/\/github\.com\/ header-replace Accept-Language en-us
+
+[Mitm]
+hostname = raw.githubusercontegithubnt.com,gist.githubusercontent.com,github.com
